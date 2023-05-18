f19143b6c480c2c3a88fc776700d7dbd4a88f97d
MiguelEsqueda <pancho-miguel@hotmail.com>
2023-05-18 09:35:43 -0600

    Añadido login basico

--- routes/Usuarios/usuario.js
+++ routes/Usuarios/usuario.js
@@ -9,6 +9,19 @@ router.get("/datos/:uid",async (req,res,next)=>{
     }
     return res.json(usuario);
 })
+router.post("/datos/log",async (req,res,next)=>{
+    console.log(req.body)
+    let usuario = await Usuario.query().findOne({email:req.body.email});
+    if(!usuario){
+        return res.status(404).json("Usuario no encontrado en nuestra base de datos");
+    }
+    if(usuario.email && !usuario.contrasena){
+        return res.status(500).json("Usuario registrado con google")
+    }
+    if(usuario.email && usuario.contrasena!=req.body.contrasena)
+        return res.status(500).json("Contraseña incorrecta");
+    return res.json(usuario);
+})
 
 
 module.exports = router;
