8e7b6ff873ee4111221d6f2329db3f2e840bfbb8
Gabo-Puck <melkorgodo@gmail.com>
2023-05-29 17:42:45 -0600

    Modificado: Ahora la ruta usuario/reestablecerContrasena regresa un status 409 si es que la contraseña ingresada es la misma que la contraseña actual de la cuenta

--- routes/usuario.js
+++ routes/usuario.js
@@ -543,7 +543,11 @@ router.get("/validarLink/:stringEncoded", async (req, res, next) => {
       .status(400)
       .json("Dato encriptado no coincide con los parametros");
   }
-  if (fechaExpiracion < Date.now() || usuario.cuenta_bloqueada == 0) {
+  if (
+    fechaExpiracion < Date.now() ||
+    usuario.cuenta_bloqueada == 0 ||
+    cambioContrasena == 0
+  ) {
     return res.status(401).json("El link expiro");
   }
   return res.status(200).json("Link valido");
