5f14256577b90accdc3580a416d1018b31572b8f
Gabo-Puck <melkorgodo@gmail.com>
2023-07-21 11:32:49 -0600

    Añadido: Rutas CRUD para salas

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var ticketRouter = require("./routes/ticket");
 var enviosRouter = require("./routes/envios");
 var paquetesRouter = require("./routes/paquetes");
 var ventasRouter = require("./routes/ventas");
+var salasRouter = require("./routes/sala");
 var fileUpload = require("express-fileupload");
 var { knex } = require("./setup/knexfile");
 
@@ -83,6 +84,7 @@ app.use("/envios", enviosRouter);
 app.use("/ticket", ticketRouter);
 app.use("/paquetes", paquetesRouter);
 app.use("/ventas", ventasRouter);
+app.use("/salas", salasRouter);
 app.use("/", indexRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
